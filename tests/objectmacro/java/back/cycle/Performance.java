88c70beeb669075f4b433d15333d1bdb9bfbef1b
Lâm TRAN VAN BA <vuanhlam.tranvanba@gmail.com>
2018-04-11 15:58:15 -0400

    Add time measure in macros
    Signed-off-by: Lâm TRAN VAN BA <vuanhlam.tranvanba@gmail.com>

--- tests/objectmacro/java/back/cycle/Performance.java
+++ tests/objectmacro/java/back/cycle/Performance.java
@@ -2,15 +2,28 @@ package back.cycle;
 
 import graph_generator.GraphGenerator;
 
+import java.lang.reflect.InvocationTargetException;
+
+
 public class Performance {
 
+    public static long endTime;
+
     public static void main(String[] args){
-        long start = System.nanoTime();
-        GraphGenerator.exponantialCase(Performance.class.getPackage().getName(), 1000);
-        long end = System.nanoTime();
-        System.out.println("Total time taken : " + ((end - start)/ 1000000));
+        long max_long = 0;
 
+        for(int i = 0; i < 100; i++){
+            GraphGenerator.exponantialCase(Performance.class.getPackage().getName(), 2000);
+        }
 
+        for(int i = 0; i < 10; i++){
+            long endTime = GraphGenerator.exponantialCase(Performance.class.getPackage().getName(), 3000);
+            System.out.println("End time taken : " + endTime);
+            max_long += endTime;
+        }
+
+        System.out.println("Total time taken : " + max_long);
+        System.out.println("Average total time taken : " + max_long / 10);
     }
 
 }
--- utils/run-cycle-tests.sh
+++ utils/run-cycle-tests.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
+java -jar lib/objectmacro.jar -d tests/objectmacro/common/intermediate/ tests/objectmacro/common/back-tests/cycle2.objectmacro
+java -jar lib/objectmacro-back.jar -d tests/objectmacro/java -p back.cycle.macro tests/objectmacro/common/intermediate/cycle2.intermediate
 java -jar lib/objectmacro.jar -d tests/objectmacro/common/intermediate/ tests/objectmacro/common/graph_generator.objectmacro
-java -jar lib/objectmacro-back.jar -d tests/objectmacro/java -p graph_generator.macro tests/objectmacro/common/intermediate/graphe_generator.intermediate
+java -jar lib/objectmacro-back.jar -d tests/objectmacro/java -p graph_generator.macro tests/objectmacro/common/intermediate/graph_generator.intermediate
 javac -cp tests/objectmacro/java/ -d classes/ tests/objectmacro/java/back/*.java tests/objectmacro/java/back/cycle/*.java
 java -cp classes/ back.cycle.Performance
